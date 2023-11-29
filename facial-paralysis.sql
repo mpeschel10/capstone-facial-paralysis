92d1dbe4e30e4c79a5a90fdac546e89a72e14728
Mark Peschel <mpeschel10@gmail.com>
2023-11-29 18:16:30 -0500

    Add user alias table

--- facial-paralysis.sql
+++ facial-paralysis.sql
@@ -1,5 +1,6 @@
 -- mariadb --user=test_user --password=password -D fp < facial-paralysis.sql
 
+DROP TABLE IF EXISTS user_aliases;
 DROP TABLE IF EXISTS sent_files;
 DROP TABLE IF EXISTS users;
 
@@ -15,12 +16,26 @@ INSERT INTO users
     (username, password, kind)
 VALUES
     ('mpeschel10', 'password', 'ADMIN'),
+    ('jcarson', 'password', 'ADMIN'),
+    ('jmiranda', 'password', 'ADMIN'),
     ('anon1', 'password', 'USER')
 ;
 
 CREATE TABLE sent_files (
     id INT AUTO_INCREMENT,
-    CONSTRAINT PRIMARY KEY (id)
+    url VARCHAR(255),
+    user_id INT,
+    CONSTRAINT PRIMARY KEY (id),
+    CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE user_aliases (
+    id INT AUTO_INCREMENT,
+    user_primary INT,
+    user_alias INT,
+    CONSTRAINT PRIMARY KEY (id),
+    CONSTRAINT FOREIGN KEY (user_primary), REFERENCES users (id),
+    CONSTRAINT FOREIGN KEY (user_alias), REFERENCES users (id)
 );
 
 SELECT * FROM users;
