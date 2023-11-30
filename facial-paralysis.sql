02203857305f2d985be1206d5c97615d3def57c0
Mark Peschel <mpeschel10@gmail.com>
2023-11-30 00:33:54 -0500

    Add messages table; rename aliases table.

--- facial-paralysis.sql
+++ facial-paralysis.sql
@@ -1,5 +1,6 @@
 -- mariadb --user=test_user --password=password -D fp < facial-paralysis.sql
 
+DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS user_aliases;
 DROP TABLE IF EXISTS sent_files;
 DROP TABLE IF EXISTS users;
@@ -22,22 +23,30 @@ VALUES
 ;
 
 CREATE TABLE sent_files (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255),
     note TEXT,
     user_id INT,
     date BIGINT,
-    CONSTRAINT PRIMARY KEY (id),
     CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE TABLE user_aliases (
-    id INT AUTO_INCREMENT,
-    user_primary INT,
-    user_alias INT,
-    CONSTRAINT PRIMARY KEY (id),
-    CONSTRAINT FOREIGN KEY (user_primary) REFERENCES users (id),
-    CONSTRAINT FOREIGN KEY (user_alias) REFERENCES users (id)
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    note TEXT,
+    sender_id INT,
+    recipient_id INT,
+    status SET('SENT', 'DELIVERED'),
+    CONSTRAINT FOREIGN KEY (sender_id) REFERENCES users (id),
+    CONSTRAINT FOREIGN KEY (recipient_id) REFERENCES users (id)
+);
+
+CREATE TABLE aliases (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    primary_id INT,
+    alias_id INT,
+    CONSTRAINT FOREIGN KEY (primary_id) REFERENCES users (id),
+    CONSTRAINT FOREIGN KEY (alias_id) REFERENCES users (id)
 );
 
 SELECT * FROM users;
