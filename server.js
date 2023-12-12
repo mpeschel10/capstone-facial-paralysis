61d735adf1f0a827a64eabcead28731d04843ab6
Mark Peschel <mpeschel10@gmail.com>
2023-12-12 11:28:00 -0500

    Add express.js file server.

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Documented https://expressjs.com/en/starter/static-files.html
+app.use(express.static('fa'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
+
