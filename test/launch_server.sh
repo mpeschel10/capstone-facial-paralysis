189bc956a0cb333a785508314f7a5263ff3ce381
Mark Peschel <mpeschel10@gmail.com>
2023-12-24 14:56:52 -0500

    Move test-prep to package.json

--- test/launch_server.sh
+++ test/launch_server.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-systemctl is-active --quiet mariadb || sudo systemctl start mariadb
-. secrets/secrets.env # Load passwords and keys into environment variables.
-
 # To allow the test.py script to shut down the server,
 #  save our process id to test/run/server_pid.
 mkdir -p test/run
 echo $$ > test/run/server_pid
 
-npm run dev
+npm run dev-mark
