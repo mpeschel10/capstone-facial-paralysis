e56207230815539222cdd15606c854ee1f5d0a2c
Mark Peschel <mpeschel10@gmail.com>
2024-01-08 12:35:54 -0500

    + file ids, submit stuff to /messages/new Clean up responses.js a little. test_e2e_upload now gets as far as submitting but is unauthenticated; need login method. We're cookies only now, I think, and BadToken is unused.

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,14 +1,15 @@
 export default function MessagesNew() {
     return (<div>
         <h2>New message</h2>
-        <form>
-        <label>At rest <input type="file"></input></label><br />
-        <label>Eyebrows up <input type="file"></input></label><br />
-        <label>Eyes closed <input type="file"></input></label><br />
-        <label>Wrinkled nose <input type="file"></input></label><br />
-        <label>Big smile <input type="file"></input></label><br />
-        <label>Lips puckered <input type="file"></input></label><br />
-        <label>Lower teeth bared <input type="file"></input></label><br />
+        <form encType="multipart/form-data" method="POST" action="/api/image">
+            <label>At rest <input type="file" id="file-at-rest"></input></label><br />
+            <label>Eyebrows up <input type="file" id="file-eyebrows-up"></input></label><br />
+            <label>Eyes closed <input type="file" id="file-eyes-closed"></input></label><br />
+            <label>Wrinkled nose <input type="file" id="file-nose-wrinkle"></input></label><br />
+            <label>Big smile <input type="file" id="file-big-smile"></input></label><br />
+            <label>Lips puckered <input type="file" id="file-lips-puckered"></input></label><br />
+            <label>Lower teeth bared <input type="file" id="file-lower-teeth-bared"></input></label><br />
+            <button id="submit-message" type="submit">Send files</button>
         </form>
     </div>)
 }
--- test/test_e2e.mjs
+++ test/test_e2e.mjs
@@ -4,11 +4,36 @@ const readline = readlinePackage.createInterface({
     output: process.stdout,
 });
 
+import path from "node:path";
+const rootDir = path.resolve(".");
+const resourcesDir = path.join(rootDir, "test", "resources");
+
 import { Builder, By } from "selenium-webdriver";
 import firefox from "selenium-webdriver/firefox.js";
 
 
+const SERVER_URL = "http://localhost:3000";
+
+const screen = {
+    width: 640,
+    height: 480
+};
+
 let driver = null;
+function getDriver({headless}) {
+    let driverPromise = new Builder().forBrowser("firefox");
+    if (headless) {
+        driverPromise = driverPromise
+            .setFirefoxOptions(new firefox.Options().headless().windowSize(screen));
+    }
+    console.info("Getting driver...");
+    return driverPromise.build();
+}
+
+async function login(driver, username, password) {
+    // TODO
+}
+
 
 // Utility I use while writing tests. 
 function fetchEnter() { return new Promise(resolve => {
@@ -30,15 +55,6 @@ const parser = {
 
 
 
-const SERVER_URL = "http://localhost:3000";
-
-const screen = {
-    width: 640,
-    height: 480
-};
-
-
-
 async function test_e2e_login() {
     const endpoint = SERVER_URL + '/login';
     console.debug("Get", endpoint);
@@ -47,15 +63,33 @@ async function test_e2e_login() {
     await driver.findElement(By.id('text-username')).sendKeys('mpeschel');
     console.debug("Fill in password");
     await driver.findElement(By.id('password-password')).sendKeys('mpeschel_password');
+    console.debug("Cookies:", await driver.manage().getCookies());
 }
 
 async function test_e2e_upload() {
-    const endpoint = SERVER_URL + '/home';
+    await login(driver, "mpeschel", "mpeschel_password");
+    
+    const endpoint = SERVER_URL + "/home";
     console.debug("Get", endpoint);
     await driver.get(endpoint);
     console.debug("Click link to new progress report");
-    await driver.findElement(By.id('a-progress-new')).click();
+    await driver.findElement(By.id("a-progress-new")).click();
     
+    const reportParts = [
+        ["at", "rest"], ["big", "smile"], ["eyebrows", "up"], ["eyes", "closed"],
+        ["lips", "puckered"], ["lower", "teeth", "bared"], ["nose", "wrinkle"],
+    ];
+
+    console.debug("Attach images to form.")
+    for (const reportPart of reportParts) {
+        const inputId = "file-" + reportPart.join("-");
+        const imagePath = path.join(resourcesDir, `mpeschel_${reportPart.join("_")}.jpg`);
+        await driver.findElement(By.id(inputId)).sendKeys(imagePath);
+    }
+    
+    console.debug("Click submit");
+    await driver.findElement(By.id("submit-message")).click();
+
     await fetchEnter();
 
     // I would expect something like, eight placeholder images (sketches) to indicate appearence.
@@ -75,13 +109,7 @@ async function main() {
     console.log(args.headless);
     console.log(args.full);
     
-    let driverPromise = new Builder().forBrowser("firefox");
-    if (args.headless) {
-        driverPromise = driverPromise
-            .setFirefoxOptions(new firefox.Options().headless().windowSize(screen));
-    }
-    console.info("Getting driver...");
-    driver = await driverPromise.build();
+    driver = await getDriver(args);
     
     try {
         let tests = [
