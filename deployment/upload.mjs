4bc0965829e0a78d9d53d6f9a17e92638b1c36dc
Mark Peschel <mpeschel10@gmail.com>
2024-01-27 15:38:26 -0500

    Add config files for nginx.

--- deployment/upload.mjs
+++ deployment/upload.mjs
@@ -17,27 +17,31 @@ function liveExec(program, args) {
 }
 
 const commands = [
-    // ['ssh', [REMOTE, 'echo "Yes, I think"'], 'Sanity check. Are we logged in?'],
+    // ['ssh', [REMOTE, 'echo "Yes, I think we\'re logged in."'], 'Sanity check. Are we logged in?'],
     // ['ssh', [REMOTE, 'apt-get update'], 'Get list of packages to upgrade.'],
     // ['ssh', [REMOTE, 'apt-get upgrade -y'], 'Blindly upgrade everything. Hope nothing breaks...'],
     
-    // ['git', ['remote', 'add', 'test', REMOTE_TEST]],
-    // ['git', ['remote', 'set-url', 'test', REMOTE_TEST]],
+    // ['git', ['remote', 'add', 'test', REMOTE_TEST], 'Tell next.js repo the server\'s address for the first time.'],
+    // ['git', ['remote', 'set-url', 'test', REMOTE_TEST], 'Tell next.js repo the server\'s address.'],
     
-    // ['ssh', [REMOTE, 'mkdir', '-p', '/opt/fa-test/']],
-    // ['ssh', [REMOTE, 'cd /opt/fa-test && git init']],
-    // ['git', ['push', 'test', 'main']],
-    // ['ssh', [REMOTE, 'cd /opt/fa-test && git checkout -b production']],
+    // ['ssh', [REMOTE, 'mkdir', '-p', '/opt/fa-test/'], 'Create folder for next.js server so we can push to it.'],
+    // ['ssh', [REMOTE, 'cd /opt/fa-test && git init'], 'Create git repository for next.js server so we can push to it.'],
+    // ['git', ['push', 'test', 'main'], 'Stage next.js main branch on server for the first time.'],
+    // ['ssh', [REMOTE, 'cd /opt/fa-test && git checkout -b production'], 'Create new production branch because git dislikes pushing to main while main is checked out.'],
     
-    ['ssh', [REMOTE, 'cd /etc/systemd/system && ln -s /opt/fa-test/deployment/fa-test-server.service']],
-    ['ssh', [REMOTE, 'systemctl daemon-reload']],
-    ['ssh', [REMOTE, 'systemctl enable --now fa-test-server']],
+    // ['ssh', [REMOTE, 'cd /etc/systemd/system && ln -s /opt/fa-test/deployment/fa-test-server.service'], 'Tell systemd where fa-test-server.service is.'],
+    // ['ssh', [REMOTE, 'systemctl daemon-reload'], 'Reload fa-test-server.service in case it changed.'],
+    // ['ssh', [REMOTE, 'systemctl enable --now fa-test-server'], 'Set next.js to run on boot.'],
 
-    // ['git', ['push', 'test', 'main']],
-    // ['ssh', [REMOTE, 'cd /opt/fa-test && git merge main']],
-    ['ssh', [REMOTE, 'cd /opt/fa-test && npm i']],
-    ['ssh', [REMOTE, 'cd /opt/fa-test && npm run build']],
-    ['ssh', [REMOTE, 'systemctl restart fa-test-server']],
+    ['ssh', [REMOTE, 'cd /etc/nginx/sites-available && ln -s /opt/fa-test/deployment/facial-analytics.conf'], 'Tell nginx where facial-analytics.conf is.'],
+    ['ssh', [REMOTE, 'cd /etc/nginx/sites-enabled && ln -s /etc/nginx/sites-available/facial-analytics.conf'], 'Enable facial-analytics in nginx.'],
+
+    ['git', ['push', 'test', 'main'], 'Stage next.js main branch on server.'],
+    ['ssh', [REMOTE, 'cd /opt/fa-test && git merge main'], 'Merge staged main branch into live production.'],
+    // ['ssh', [REMOTE, 'cd /opt/fa-test && npm i'], 'NPM install next.js server.']',
+    // ['ssh', [REMOTE, 'cd /opt/fa-test && npm run build'], 'Build next.js server.'],
+    // ['ssh', [REMOTE, 'systemctl restart fa-test-server'], 'Restart next.js server.'],
+    ['ssh', [REMOTE, 'nginx -t && nginx -s reload'], 'Restart Nginx.'],
 ];
 
 for (const [program, args, description] of commands) {
