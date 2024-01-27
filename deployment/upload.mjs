bd412a8c271a94b665f86900ec7d2a167828aac2
Mark Peschel <mpeschel10@gmail.com>
2024-01-27 14:34:34 -0500

    + Upload main branch of repository

--- deployment/upload.mjs
+++ deployment/upload.mjs
@@ -1,37 +1,34 @@
 import child_process from 'node:child_process';
 
-function exec(command) {
-    return new Promise((resolve, reject) => {
-        child_process.exec(command, (error, stdout, stderr) => {
-            if (error !== null) reject(error)
-            else resolve({stdout, stderr});
-        })
-    })
-}
+const REMOTE='root@test.fa.mpeschel10.com';
+const REMOTE_TEST=REMOTE + ':/opt/fa-test';
 
 function liveExec(program, args) {
     return new Promise((resolve, reject) => {
-        const ps = child_process.spawn(program, args);
-        ps.stdout.on('data', data => process.stdout.write(data));
-        ps.stdout.on('close', () => resolve());
-        ps.stderr.on('data', data => process.stderr.write(data));
+        try {
+            const ps = child_process.spawn(program, args);
+            ps.stdout.on('data', data => process.stdout.write(data));
+            ps.stdout.on('close', () => resolve());
+            ps.stderr.on('data', data => process.stderr.write(data));
+        } catch(error) {
+            reject(error);
+        }
     })
 }
 
 const commands = [
-    ['Sanity check. Are we logged in?', 'echo "Yes, I think"'],
-    // ['Get list of packages to upgrade.', 'apt-get update'],
-    // ['Blindly upgrade everything. Hope nothing breaks...', 'apt-get upgrade -y'],
+    ['ssh', [REMOTE, 'echo "Yes, I think"'], 'Sanity check. Are we logged in?'],
+    ['ssh', [REMOTE, 'apt-get update'], 'Get list of packages to upgrade.'],
+    ['ssh', [REMOTE, 'apt-get upgrade -y'], 'Blindly upgrade everything. Hope nothing breaks...'],
+    ['ssh', [REMOTE, 'mkdir', '-p', '/opt/fa-test/']],
+    // ['ssh', [REMOTE, 'cd /opt/fa-test && git init']],
+    // ['git', ['remote', 'add', 'test', REMOTE_TEST]],
+    ['git', ['remote', 'set-url', 'test', REMOTE_TEST]],
+    ['git', ['push', 'test', 'main']],
 ];
 
-// console.log(await exec(`ssh fa-test ${commands}`));
-// console.log(await liveExec('du', ['/']))
-
-for (const [description, command] of commands) {
-    console.log('Doing:', description);
-    await liveExec('ssh', ['fa-test', command]);
+for (const [program, args, description] of commands) {
+    if (description) console.log('Doing:', description);
+    await liveExec(program, args);
     console.log();
 }
-
-
-
